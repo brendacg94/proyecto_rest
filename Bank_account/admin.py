c1275799ef1da2bfe28ecee41d9ee5dd720665db
brendacg <gm12099@ues.edu.sv>
2022-02-08 23:08:29 -0600

    subiendo los archivos

---
+++ Bank_account/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from Bank_account.models import *
+
+# Register your models here.
+admin.site.register(Bank_account)
---
+++ users/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+# Create your models here.
+
+class UsuarioManager(BaseUserManager):
+
+    def create_user(self,email,names,last_names, password = None):
+        if not email:
+            raise ValueError('¡El usuario debe tener un correo electrónico!')
+
+        usuario = self.model(
+            email = self.normalize_email(email), 
+            names = names, 
+            last_names = last_names
+        )
+
+        # Encripta la contraseña y la guarda en ese mismo campo
+        usuario.set_password(password)
+        usuario.save()
+        return usuario 
+
+    def create_superuser(self,email,names,last_names,password):
+        usuario = self.create_user(
+            email, 
+            names = names, 
+            last_names = last_names,
+            password = password
+        )
+
+        usuario.admin_user = True
+        usuario.save()
+        return usuario 
+
+class Usuario(AbstractBaseUser):
+   # username = models.CharField('Nombre de usuario', unique = true, max_length = 100)
+    email = models.EmailField('Correo electrónico', unique = True, max_length = 254)
+    names = models.CharField('Nombres', max_length = 255, blank = False, null = False)
+    last_names = models.CharField('Apellidos', max_length = 255, blank = False, null = False)
+    active_user = models.BooleanField(default = True) # todo usuario que tenga este campo en true puede iniciar sesion
+    admin_user = models.BooleanField(default = False) # identificar que usuario va poder iniciar sesion en el administrador de django y que usuario no
+    objects = UsuarioManager()
+
+    # Hace referencia al parametro unico que va a diferenciar al usuario, que siempre va a ser requerido
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['names','last_names']
+
+    def __str__(self):
+        return f'{self.names},{self.last_names}'
+
+    def has_perm(self,perm,obj = None):
+        return True
+
+    def has_module_perms(self,app_label):
+        return True
+
+    # Verifica si un usuario es administrador o no 
+    @property 
+    def is_staff(self):
+        return self.admin_user
+
+
+
+
+    
+
