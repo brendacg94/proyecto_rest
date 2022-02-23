d655ee75b74630014e7417e734d28b61b9ca4819
brendacg <gm12099@ues.edu.sv>
2022-02-22 22:19:51 -0600

    elimino archivos innecesarios

--- Bank_account/routers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from Bank_account.bank_account_viewsets import Bank_accountViewSet
-
-router = DefaultRouter()
-router.register(r'bank_accounts', Bank_accountViewSet, basename='bank_account')
-urlpatterns = router.urls
